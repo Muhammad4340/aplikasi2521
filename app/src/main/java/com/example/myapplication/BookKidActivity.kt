3700e5ec521eeb74d37d05f579a8f1e60a0c541f
Muhammad4340 <rafibaureno2@gmail.com>
2023-10-13 17:53:27 +0700

    13 oktober 2023

--- app/src/main/java/com/example/myapplication/BookKidActivity.kt
+++ app/src/main/java/com/example/myapplication/BookKidActivity.kt
@@ -13,7 +13,7 @@ class BookKidActivity : AppCompatActivity() {
     lateinit var binding: ActivityBookKidBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityBookKidBinding.inflate(layoutInflater)
+        binding = ActivityBookKidBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //event button utk call fragment
         binding.buttonFairy.setOnClickListener {
@@ -26,17 +26,14 @@ class BookKidActivity : AppCompatActivity() {
         binding.buttonFairy.setOnClickListener {
             replaceFragment(ScienceFragment())
 
-    //ganti fragment
-    fun replaceFragment(frg:Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmenTrx = fragmentManager.beginTransaction()
-        fragmenTrx.replace(R.id.fragmentKidBook, frg)
-        fragmenTrx.commit()
+            //ganti fragment
+            fun replaceFragment(frg: Fragment) {
+                val fragmentManager = supportFragmentManager
+                val fragmenTrx = fragmentManager.beginTransaction()
+                fragmenTrx.replace(R.id.fragmentKidBook, frg)
+                fragmenTrx.commit()
 
-        //instance
-        val cal:ImageView = findViewById(R.Id.imageViewCall)
-        val cal:ImageView = findViewById(R.Id.imageViewCall)
-        val cal:ImageView = findViewById(R.Id.imageViewCall)
-
+            }
+        }
     }
 }
