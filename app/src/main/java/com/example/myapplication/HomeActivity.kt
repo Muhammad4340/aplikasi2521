7d0a58be3c82c79f5e4aefebe99ae1d3d9c50464
Muhammad4340 <rafibaureno2@gmail.com>
2023-10-16 17:11:48 +0700

    16 oktober 2023

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //instance
+        val btnMore:Button = findViewById(R.id.Menu4)
+        val btnKid:Button = findViewById(R.id.Menu1)
+        val rvBuku:RecyclerView = findViewById(R.id.recyclerViewBuku)
+        //set layout manager di RecyclerView
+        rvBuku.layoutManager = LinearLayoutManager(this)
+        //list data buku
+        val data = ArrayList<ImageView>()
+        data.add(ImageView(R.drawable.book1,"Emi's beach adventure",
+            "Lorem Ipsum Dolor Sit Amet"))
+        data.add(ImageView(R.drawable.book2,"Ade's beach adventure",
+            "Lorem Ipsum Dolor Sit Amet"))
+        data.add(ImageView(R.drawable.book4,"Mermaid to Rescue",
+            "Lorem Ipsum Dolor Sit Amet"))
+
+        //set adapter
+        val adapter = AdapterHome(data)
+        //set adapter ke recycler view
+        rvBuku.adapter = adapter
     }
 }
